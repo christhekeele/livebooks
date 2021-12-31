f8f39547f9305872a6b42fe0066d229454a41e60
Chris Keele <christhekeele@gmail.com>
2021-12-30 22:22:15 -0500

    Fix bad version spec typo.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Livebooks.MixProject do
     [
       app: :livebooks,
       version: "0.1.0",
-      elixir: "~> 1.",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
